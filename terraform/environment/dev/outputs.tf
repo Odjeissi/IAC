9ee00cd06a3bf7f5ea47a0cc8d8a77d5d48a1571
Odjeissi Mendes <odjeissi@gmail.com>
2026-06-21 00:12:57 -0400

    update outputs.tf

--- terraform/environment/dev/outputs.tf
+++ terraform/environment/dev/outputs.tf
@@ -1,1 +1,16 @@
+output "alb_dns_name" {
+  value = module.lb_main.lb_dns_name
+}
 
+output "route53_record" {
+  value = module.route53_main.fqdn
+}
+
+output "rds_endpoint" {
+  value     = module.db_main.db_endpoint
+  sensitive = true
+}
+
+output "s3_bucket_id" {
+  value = module.s3_main.s3_main_id
+}
